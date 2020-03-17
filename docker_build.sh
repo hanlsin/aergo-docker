2f25ce2c73c0deae161d3d0775b8fce25da0a1c7
Yun Park <hanlsin@gmail.com>
2020-03-18 03:36:03 +0900

    add latest images could be made

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-docker build --build-arg GIT_REPO="github.com/hanlsin/aergo" --build-arg GIT_BRANCH="isdb" -t hanlsin/aergo.isdb:0.1 .
+
+source ./version.sh
+
+# latest
+docker build --build-arg GIT_REPO="github.com/hanlsin/aergo" --build-arg GIT_BRANCH="isdb" -t hanlsin/aergo.isdb .
+
+docker build --build-arg GIT_REPO="github.com/hanlsin/aergo" --build-arg GIT_BRANCH="isdb" -t hanlsin/aergo.isdb:$AERGO_ISDB_VERSION .
+
--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+source ./version.sh
+
 # detach the interactive: Ctrl+p, Ctrl+q
 # reattach: docker attach myaergo
 # Interactive mode:
-#docker run --rm -it -p 7845:7845 --name=myaergo hanlsin/aergo.isdb:0.1 /bin/bash
+#docker run --rm -it -p 7845:7845 --name=myaergo hanlsin/aergo.isdb:$AERGO_ISDB_VERSION /bin/bash
+# latest version
+#docker run --rm -it -p 7845:7845 --name=myaergo hanlsin/aergo.isdb /bin/bash
 # Execute default CMD
-docker run --rm -itd -p 7845:7845 --name=myaergo hanlsin/aergo.isdb:0.1
+#docker run --rm -itd -p 7845:7845 --name=myaergo hanlsin/aergo.isdb:$AERGO_ISDB_VERSION
+# latest version
+#docker run --rm -it -p 7845:7845 --name=myaergo hanlsin/aergo.isdb
+docker run --rm -it -p 7845:7845 --name=myaergo hanlsin/aergo.isdb
 # Follow logs
-docker logs -f myaergo
+#docker logs -f myaergo
